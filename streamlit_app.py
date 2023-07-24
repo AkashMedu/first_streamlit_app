a9963d8e209c8c4ca8df836b7ef9a4f3a42c262c
AkashMedu <140011660+AkashMedu@users.noreply.github.com>
2023-07-24 13:45:10 -0700

    Update streamlit_app.py
    Remove raw line and change the request to add fruit as a kind of a variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to Display Fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 # json response to normalize
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
